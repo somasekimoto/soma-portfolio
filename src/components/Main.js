f6e52029ded0c4dc0c8723d8f3760f7a9d1fbd1e
SomaSekimoto <0421.soma@gmail.com>
2020-07-04 10:25:11 +0900

    fix: form tag

--- src/components/Main.js
+++ src/components/Main.js
@@ -121,7 +121,14 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" data-netlify="true" name="soma-contact">
+          <form
+            method="post"
+            data-netlify="true"
+            name="soma-contact"
+            netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="soma-contact" value="soma-contact" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
